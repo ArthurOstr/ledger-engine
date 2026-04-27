8734e468b47e2997b9c02974ae2568d991616fe4
ArthurOstr <automata4231@gmail.com>
2026-04-27 22:01:55 +0300

    feat: Integrating CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,9 +1,8 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database import engine, Base
-from app.models.transaction import Transaction
-
 from app.routers import transaction
 
 
@@ -16,6 +15,19 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/api/health")
