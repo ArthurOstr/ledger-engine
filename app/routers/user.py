57f802bd3984624c14b19248a34529756f6ca70a
ArthurOstr <automata4231@gmail.com>
2026-05-04 23:44:55 +0300

    feat: creating login route and giving JWT acess to user if he pass auth

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
 from app.database import get_db
 from app.models.user import User
-from app.schemas.user import UserCreate, UserResponse
-from app.core.security import get_password_hash
+from app.schemas.user import UserCreate, UserResponse, Token
+from app.core.security import get_password_hash, verify_password, create_access_token
 
 router = APIRouter(prefix="/api/users", tags=["Users"])
 
@@ -25,3 +26,23 @@ async def register_user(user: UserCreate, db: AsyncSession = Depends(get_db)):
     await db.refresh(new_user)
 
     return new_user
+
+
+@router.post("/login", response_model=Token)
+async def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)
+):
+
+    result = await db.execute(select(User).where(User.email == form_data.username))
+    user = result.scalars().first()
+
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token = create_access_token(data={"sub": str(user.email)})
+
+    return {"access_token": access_token, "token_type": "bearer"}
