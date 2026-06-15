8db87a04c91bcc51c2855a71a93802d7d445fc72
ArthurOstr <automata4231@gmail.com>
2026-06-15 22:32:59 +0300

    Creating properties with pydantic_settings to set cookies policy for dev and pro application

--- app/core/config.py
+++ app/core/config.py
@@ -1,18 +1,26 @@
-import os
 from sqlalchemy.ext.asyncio import create_async_engine
+from pydantic_settings import BaseSettings
 
-ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
-DATABASE_URL = os.getenv('DATABASE_URL')
+class Settings(BaseSettings):
+    ENVIRONMENT: str = "development"
+    DATABASE_URL: str
 
-if ENVIRONMENT == 'production':
-    engine = create_async_engine(
-        DATABASE_URL,
-       connect_args={'ssl': 'require'},
-        pool_pre_ping=True,
-    )
-else:
-    engine = create_async_engine(
-        DATABASE_URL,
-        pool_pre_ping=True,
+@property
+def is_production(self) -> bool:
+    return self.ENVIRONMENT == "production"
+
+@property
+def db_engine_kwargs(self) -> dict:
+    """Dynamically build the arg for SQLAlchemy engine kwargs"""
+    kwargs = {"pool_pre_ping": True}
+    if self.is_production:
+        kwargs["connect_args"] = {'ssl':'require'}
+    return kwargs
+
+settings = Settings()
+
+engine = create_async_engine(
+    settings.DATABASE_URL,
+    **settings.db_engine_kwargs,
     )
 
