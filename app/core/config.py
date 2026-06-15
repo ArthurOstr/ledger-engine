22cddc4cef765d3505b84009d8832d64c2422b85
ArthurOstr <automata4231@gmail.com>
2026-06-15 22:43:54 +0300

    Fixing indentation

--- app/core/config.py
+++ app/core/config.py
@@ -1,3 +1,4 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine
 from pydantic_settings import BaseSettings
 
@@ -5,17 +6,27 @@ class Settings(BaseSettings):
     ENVIRONMENT: str = "development"
     DATABASE_URL: str
 
-@property
-def is_production(self) -> bool:
-    return self.ENVIRONMENT == "production"
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
-@property
-def db_engine_kwargs(self) -> dict:
-    """Dynamically build the arg for SQLAlchemy engine kwargs"""
-    kwargs = {"pool_pre_ping": True}
-    if self.is_production:
-        kwargs["connect_args"] = {'ssl':'require'}
-    return kwargs
+    @property
+    def is_production(self) -> bool:
+        return self.ENVIRONMENT == "production"
+
+    @property
+    def cookie_secure(self):
+        return self.is_production
+
+    @property
+    def cookie_samesite(self):
+        return "none" if self.is_production else "lax"
+
+    @property
+    def db_engine_kwargs(self) -> dict:
+        """Dynamically build the arg for SQLAlchemy engine kwargs"""
+        kwargs = {"pool_pre_ping": True}
+        if self.is_production:
+            kwargs["connect_args"] = {'ssl':'require'}
+        return kwargs
 
 settings = Settings()
 
